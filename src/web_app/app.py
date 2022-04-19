44102524f6092ee9dae906368139ddfd35d0db86
Wikusa <klaskak1@tcnj.edu>
2022-04-19 16:49:46 -0400

    Created the usage query for web app

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -45,9 +45,14 @@ def result():
         rows = connect('SELECT * FROM Building WHERE name = ' + '\''+ request.form.get('building1') +'\';')
         heads = ['Name','Property ID', 'Year Built','Primary Use','Efficiency Factor','Gross Floor Area']
         return render_template('my-result.html', rows = rows, heads = heads)
-    elif request.form.get('options') == 'option2':
-        items = connect('SELECT * FROM building;')
-        return render_template('home.html', items =items)
+    elif request.form.get('options') == 'option2':\
+        sum = connect('SELECT SUM(usage) FROM meter_entry WHERE start_date > \''+ request.form.get('start_date') +'\' AND end_date < \'' + request.form.get('end_date') + '\';')
+        eff_F = connect('SELECT eff_factor FROM building WHERE name = \'' + request.form.get('building1') + '\';')
+    
+        total = float(sum[0][0])* float(eff_F[0][0])
+
+        heads = ['usage']
+        return render_template('my-result.html', total=total,heads=heads)
 
 @app.route('/query-handler', methods=['POST'])
 def query_handler():
