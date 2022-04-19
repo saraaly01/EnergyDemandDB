9e7129e2e552cc8a438e195a1070c8b4eeaff871
saraaly01 <aly.sara01@gmail.com>
2022-04-19 15:08:07 -0400

    Radio Button Options Added

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -41,9 +41,13 @@ def form():
     return render_template('home.html',items=items)
 @app.route("/results", methods=['POST']) 
 def result():
-    rows = connect('SELECT * FROM Building WHERE name = ' + '\''+ request.form.get('building1') +'\';')
-    heads = ['Name','Property ID', 'Year Built','Primary Use','Efficiency Factor','Gross Floor Area']
-    return render_template('my-result.html', rows = rows, heads = heads)
+    if request.form.get('options') == 'option1':
+        rows = connect('SELECT * FROM Building WHERE name = ' + '\''+ request.form.get('building1') +'\';')
+        heads = ['Name','Property ID', 'Year Built','Primary Use','Efficiency Factor','Gross Floor Area']
+        return render_template('my-result.html', rows = rows, heads = heads)
+    elif request.form.get('options') == 'option2':
+        items = connect('SELECT * FROM building;')
+        return render_template('home.html', items =items)
 
 @app.route('/query-handler', methods=['POST'])
 def query_handler():
